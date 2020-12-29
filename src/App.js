0b4a70c07f5d69f5c7a5d41798eb571db0dc37a7
Valentin Barit <valentin.barit@gmail.com>
2020-12-29 16:42:09 +0100

    --wip-- [skip ci]

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+import { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    fetch('http://api.open-notify.org/iss-now.json')
+      .then((response) => response.json())
+      .then((data) => {
+        const issPosition = data.iss_position;
+        console.log(issPosition);
+
+        const geojson = { center: `${issPosition.latitude},${issPosition.longitude}` };
+
+        fetch(`http://osm-static-maps.herokuapp.com?`, {
+          mode: 'no-cors',
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+          });
+      });
+  });
+
   return (
     <div>
       <header>
