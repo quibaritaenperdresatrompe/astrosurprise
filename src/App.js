5404fc185c5ebc65aa3e225585e869cee6b21dc6
Jonathan Pipard <jonathan.pipard@gmail.com>
2020-12-30 11:34:51 +0100

    feat: 🎸 MAP Setup centered on ISS position
    with ol package from openlayers.org

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import { useEffect } from 'react';
+import 'ol/ol.css';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
 
 function App() {
   useEffect(() => {
     fetch('http://api.open-notify.org/iss-now.json')
       .then((response) => response.json())
       .then((data) => {
-        const issPosition = data.iss_position;
-        console.log(issPosition);
-
-        const geojson = { center: `${issPosition.latitude},${issPosition.longitude}` };
-
-        fetch(`http://osm-static-maps.herokuapp.com?`, {
-          mode: 'no-cors',
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-          });
+        const issPosition = [Number(data.iss_position.latitude), Number(data.iss_position.longitude)];
+        const map = new Map({
+          target: 'map',
+          layers: [
+            new TileLayer({
+              source: new OSM(),
+            }),
+          ],
+          view: new View({
+            center: fromLonLat([Number(data.iss_position.longitude), Number(data.iss_position.latitude)]),
+            zoom: 5,
+          }),
+        });
       });
   });
 
@@ -24,6 +30,13 @@ function App() {
     <div>
       <header>
         <h1>Astrosurprise 🚀</h1>
+        <div
+          id="map"
+          style={{
+            width: '400px',
+            height: '250px',
+          }}
+        ></div>
       </header>
     </div>
   );
