cab0428598f50fae2fee9ebcfcf3a7669ade0ccc
Jonathan Pipard <jonathan.pipard@gmail.com>
2021-01-11 21:52:13 +0100

    refactor: 💡 WIP

--- src/LMap.js
+++ src/LMap.js
@@ -3,9 +3,14 @@ import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import { fromLonLat } from 'ol/proj';
+import Point from 'ol/geom/Point';
 
 function LMap(data) {
-  const issPosition = [Number(data.iss_position.latitude), Number(data.iss_position.longitude)];
+  let issPosition = [Number(data.iss_position.latitude), Number(data.iss_position.longitude)];
+
+  let issPoint = new Feature({
+    geometry: new Point(fromLonLat([12.5, 41.9])),
+  });
   const map = new Map({
     target: 'map',
     layers: [
